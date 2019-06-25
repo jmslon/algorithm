9496e9d2accbd167c9732c6b591898170a002240
이재영 <47062453+jmslon@users.noreply.github.com>
2019-06-25 19:18:05 +0900

    Create 6549.cpp

---
+++ boj/6549.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <vector>
+
+#define l_node (node<<1)
+#define r_node (l_node+1)
+#define mid ((begin+end)>>1)
+
+using namespace std;
+
+class SegmentTree {
+public:
+    vector<long long> arr;
+    vector<int> tree;
+    int MEANINGLESS;
+    
+    SegmentTree(int size) {
+        arr.resize(size + 1);
+        tree.resize(size * 4);
+    }
+    
+    int init(int node, int begin, int end) {
+        if (begin == end) return tree[node] = begin;
+        int l_query = init(l_node, begin, mid);
+        int r_query = init(r_node, mid+1, end);
+        return tree[node] = whichof(l_query, r_query);
+    }
+    
+    int query(int node, int begin, int end, int l_pos, int r_pos) {
+        if (end < l_pos || r_pos < begin) return MEANINGLESS;
+        if (l_pos <= begin && end <= r_pos) return tree[node];
+        int l_query = query(l_node, begin, mid, l_pos, r_pos);
+        int r_query = query(r_node, mid+1, end, l_pos, r_pos);
+        return whichof(l_query, r_query);
+    }
+    
+    long long max_area(int l_pos, int r_pos) {
+        if (l_pos > r_pos) return -1;
+        int min_pos = query(1, 1, (int)arr.size()-1, l_pos, r_pos);
+        long long ret = arr[min_pos] * (r_pos - l_pos + 1);
+        ret = max(ret, max_area(l_pos, min_pos-1));
+        ret = max(ret, max_area(min_pos+1, r_pos));
+        return ret;
+    }
+    
+    virtual int whichof(int l_idx, int r_idx) = 0;
+};
+
+struct MaxSegmentTree : public SegmentTree {
+    MaxSegmentTree(int size) : SegmentTree(size) {
+        MEANINGLESS = -1;
+    }
+    int whichof(int l_idx, int r_idx) {
+        if (l_idx == MEANINGLESS) return r_idx;
+        if (r_idx == MEANINGLESS) return l_idx;
+        return arr[l_idx] < arr[r_idx] ? l_idx : r_idx;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    int N;
+    while (1) {
+        scanf("%d", &N);
+        if (N == 0) break;
+        MaxSegmentTree segment_tree(N);
+        for (int i = 1; i <= N; ++i)
+            scanf("%lld", &segment_tree.arr[i]);
+        segment_tree.init(1, 1, N);
+        printf("%lld\n", segment_tree.max_area(1, N));
+    }
+    
+    return 0;
+}
+
