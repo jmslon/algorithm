0d3421e60cea5bada55029662beb1e00de0cfb94
이재영 <47062453+jmslon@users.noreply.github.com>
2019-07-16 22:21:03 +0900

    Update point-set.cpp

--- myalgo/point-set.cpp
+++ myalgo/point-set.cpp
@@ -16,19 +16,19 @@ typedef long long ll;
 
 class Vector {
 public:
-    int x, y;
+    ll x, y;
     Vector(){}
-    Vector(int x_, int y_) : x(x_), y(y_) {}
+    Vector(ll x_, ll y_) : x(x_), y(y_) {}
     
     bool    operator == (const Vector& a)   const {return x==a.x&&y==a.y;}
     bool    operator <  (const Vector& a)   const {return y==a.y?x<a.x:y<a.y;}
     Vector  operator +  (const Vector& a)   const {return Vector(x+a.x,y+a.y);}
     Vector  operator -  (const Vector& a)   const {return Vector(x-a.x,y-a.y);}
     Vector  operator -  (void)              const {return Vector(-x,-y);}
-    Vector  operator *  (const double a)    const {return Vector(x*a,y*a);}
+    Vector  operator *  (const ll a)        const {return Vector(x*a,y*a);}
     
-    ll dot      (const Vector& a)   const {return (ll)x*a.x+(ll)y*a.y;}
-    ll cross    (const Vector& a)   const {return (ll)x*a.y-(ll)y*a.x;}
+    ll dot      (const Vector& a)   const {return x*a.x+y*a.y;}
+    ll cross    (const Vector& a)   const {return x*a.y-y*a.x;}
     double size (void)              const {return sqrt(dot(*this));}
     ll size2    (void)              const {return dot(*this);}
 };
@@ -46,54 +46,48 @@ struct PointSet {
 };
 
 struct FarestPair : public PointSet {
-    vector<Vector> cvx;
+    vector<Vector> hull;
     vector<Vector> farest_pair;
     
     FarestPair(int size) : PointSet(size) {}
     
     virtual void sort_points() {
         // select (O)rigin and swap P[0]
-        int o_idx = 0;
-        for (int i = 1; i < size; ++i)
-            if (!(P[o_idx] < P[i]))
-                o_idx = i;
-        swap(P[0], P[o_idx]);
+        swap(P[0], *min_element(P.begin(), P.end()));
         O = P[0];
         
         // sort Points by O (counter clock wise).
-        for (int i = 1; i < size; ++i)
-            P[i] = P[i] - O;
+        for (int i = 1; i < size; ++i) P[i] = P[i] - O;
         sort(P.begin()+1, P.end(), [] (const Vector &a, const Vector &b) {
             ll cr = a.cross(b);
             if(cr) return cr > 0;
             return a.y + abs(a.x) < b.y + abs(b.x);
         });
-        for (int i = 1; i < size; ++i)
-            P[i] = P[i] + O;
+        for (int i = 1; i < size; ++i) P[i] = P[i] + O;
     }
     
     void graham_scan() {
-        cvx = {O};
+        hull = {O};
         for (int i = 1; i < size; ++i) {
             while (1) {
-                if (cvx.size() <= 1) break;
-                Vector back1 = cvx.back(), back2 = cvx[cvx.size()-2];
+                if (hull.size() <= 1) break;
+                Vector back1 = hull.back(), back2 = hull[hull.size()-2];
                 if ((back1-back2).cross(P[i]-back2) > 0) break;
-                cvx.pop_back();
+                hull.pop_back();
             }
-            cvx.push_back(P[i]);
+            hull.push_back(P[i]);
         }
     }
     
     void rotating_calipers() {
-        ll max = 0;
-        Vector A, B;
-        for (int i = 0, j = 1; i < cvx.size(); ++i) {
-            int ni = (i+1) % cvx.size();
-            Vector NI = cvx[ni], I = cvx[i], NJ, J;
+        Vector A = hull[0], B = hull[1];
+        ll max = (A-B).size2();
+        for (int i = 0, j = 1; i < hull.size(); ++i) {
+            int ni = (i+1) % hull.size();
+            Vector NI = hull[ni], I = hull[i], NJ, J;
             while (1) {
-                int nj = (j+1) % cvx.size();
-                NJ = cvx[nj]; J = cvx[j];
+                int nj = (j+1) % hull.size();
+                NJ = hull[nj]; J = hull[j];
                 if ((NI-I).cross(NJ-J) > 0) j = nj;
                 else break;
             }
@@ -119,7 +113,6 @@ struct ClosestPair : public PointSet {
         set<Vector> SET = {P[0], P[1]};
         Vector A = P[0], B = P[1];
         ll min = (A-B).size2();
-        
         for (int i = 2, j = 0; i < size; ++i) {
             Vector I = P[i];
             for (; j < i; ++j) {
