5ce96cb38a9a688434bab9fd4d058b73b161cf24
이재영 <47062453+jmslon@users.noreply.github.com>
2019-08-15 02:07:11 +0900

    Update point-set.cpp

--- myalgo/point-set.cpp
+++ myalgo/point-set.cpp
@@ -11,26 +11,46 @@
 
 using namespace std;
 
-typedef double ll;
+typedef long long ll;
 
 struct Vector {
     ll x, y;
     Vector(){}
-    Vector(ll _x, ll _y) : x(_x), y(_y) {}
+    Vector(ll _x,ll _y):x(_x),y(_y){}
     
-    bool    operator == (const Vector& a)   const {return x==a.x&&y==a.y;}
-    bool    operator <  (const Vector& a)   const {return y==a.y?x<a.x:y<a.y;}
-    Vector  operator +  (const Vector& a)   const {return {x+a.x,y+a.y};}
-    Vector  operator -  (const Vector& a)   const {return {x-a.x,y-a.y};}
+    bool    operator == (const Vector &a)   const {return x==a.x&&y==a.y;}
+    bool    operator <  (const Vector &a)   const {return y==a.y?x<a.x:y<a.y;}
+    Vector  operator +  (const Vector &a)   const {return {x+a.x,y+a.y};}
+    Vector  operator -  (const Vector &a)   const {return {x-a.x,y-a.y};}
     Vector  operator -  (void)              const {return {-x,-y};}
     Vector  operator *  (const ll a)        const {return {x*a,y*a};}
     
-    ll dot      (const Vector& a)   const {return x*a.x+y*a.y;}
-    ll cross    (const Vector& a)   const {return x*a.y-y*a.x;}
-    double size (void)              const {return sqrt(x*x+y*y);}
-    ll size2    (void)              const {return x*x+y*y;}
+    ll dot       (const Vector &a)   const {return x*a.x+y*a.y;}
+    ll cross     (const Vector &a)   const {return x*a.y-y*a.x;}
+    double size  (void)              const {return sqrt(x*x+y*y);}
+    ll size2     (void)              const {return x*x+y*y;}
     
-    void print_info() {printf("(%.9lf, %.9lf)\n", x, y);}
+    void print_info() {cout<<"("<<x<<","<<y<<")\n";}
+};
+
+struct Vector3: Vector {
+    ll z;
+    Vector3(){}
+    Vector3(ll _x, ll _y, ll _z):Vector(_x, _y),z(_z){}
+    
+    bool    operator == (const Vector3 &a)  const {return x==a.x&&y==a.y&&z==a.z;}
+    bool    operator <  (const Vector3 &a)  const {return z==a.z?y==a.y?x<a.x:y<a.y:z<a.z;}
+    Vector3 operator +  (const Vector3 &a)  const {return {x+a.x,y+a.y,z+a.z};}
+    Vector3 operator -  (const Vector3 &a)  const {return {x-a.x,y-a.y,z-a.z};}
+    Vector3 operator -  (void)              const {return {-x,-y,-z};}
+    Vector3 operator *  (const ll a)        const {return {x*a,y*a,z*a};}
+    
+    ll dot        (const Vector3 &a)  const {return x*a.x+y*a.y+z*a.z;}
+    Vector3 cross (const Vector3 &a)  const {return {y*a.z-z*a.y,z*a.x-x-a.z,x*a.y-y*a.x};}
+    double size   (void)              const {return sqrt(x*x+y*y+z*z);}
+    ll size2      (void)              const {return x*x+y*y+z*z;}
+    
+    void print_info() {cout<<"("<<x<<","<<y<<","<<z<<"\n";}
 };
 
 struct LineSegment {
@@ -83,7 +103,7 @@ struct LineSegment {
         t1 /= det;
         t2 /= det;
         if (0<=t1&&t1<=1&&0<=t2&&t2<=1) {
-//            return P.first + p * t1;
+            //return P.first + p * t1;
             return a.P.first + q * t2;
         } else return VOID_VECTOR;
     }
@@ -99,26 +119,25 @@ struct PointSet {
     virtual void sort_points() = 0;
 };
 
-struct FarestPair : public PointSet {
+struct FarestPair: PointSet {
     vector<Vector> hull;
-    vector<pair<Vector, Vector> > farest_pairs;
+    vector<pair<Vector, Vector> > pairs;
     
-    FarestPair(int size) : PointSet(size) {
-        P.resize(size);
-    }
+    FarestPair(int size): PointSet(size) {}
     
     virtual void sort_points() {
         swap(P[0], *min_element(P.begin(), P.end()));
         for (int i = 1; i < P.size(); ++i) P[i] = P[i] - P[0];
         sort(P.begin()+1, P.end(), [] (const Vector &a, const Vector &b) {
             ll cr = a.cross(b);
-            if(cr) return cr > 0;
+            if (cr) return cr > 0;
             return a.y + abs(a.x) < b.y + abs(b.x);
         });
         for (int i = 1; i < P.size(); ++i) P[i] = P[i] + P[0];
     }
     
     void graham_scan() {
+        //sort_points();
         hull = {P[0]};
         for (int i = 1; i < P.size(); ++i) {
             while (1) {
@@ -131,7 +150,9 @@ struct FarestPair : public PointSet {
         }
     }
     
-    ll rotating_calipers() {
+    void rotating_calipers() {
+        //graham_scan();
+        pairs = {{hull[0], hull[1]}};
         ll max = (hull[0]-hull[1]).size2();
         for (int i = 0, j = 1; i < hull.size(); ++i) {
             int ni = (i+1) % hull.size();
@@ -144,28 +165,115 @@ struct FarestPair : public PointSet {
             }
             ll tmp = (I-J).size2();
             if(max < tmp) {
-                farest_pairs = {{I, J}};
+                pairs = {{I, J}};
                 max = tmp;
             } else if (max == tmp) {
-                farest_pairs.push_back({I, J});
+                pairs.push_back({I, J});
             }
         }
-        return max;
+        for (auto pair: pairs) if (pair.first < pair.second) swap(pair.first, pair.second);
+        sort(pairs.begin(), pairs.end());
+        pairs.erase(unique(pairs.begin(), pairs.end()), pairs.end());
+    }
+    
+    void brute_force() {
+        pairs = {{P[0], P[1]}};
+        ll max = (P[0]-P[1]).size2();
+        for (int i = 0; i < P.size(); ++i) {
+            Vector I = P[i];
+            for (int j = i + 1; j < P.size(); ++j) {
+                Vector J = P[j];
+                ll tmp = (I-J).size2();
+                if (max < tmp) {
+                    pairs = {{I, J}};
+                    max = tmp;
+                } else if (max == tmp) {
+                    pairs.push_back({I, J});
+                }
+            }
+        }
     }
 };
-int main() {
-    ll x[4], y[4];
-    scanf("%lf %lf %lf %lf %lf %lf %lf %lf", x, y, x+1, y+1, x+2, y+2, x+3, y+3);
-    LineSegment L1({{x[0], y[0]}, {x[1], y[1]}});
-    LineSegment L2({{x[2], y[2]}, {x[3], y[3]}});
+
+struct ClosestPair: PointSet {
+    vector<pair<Vector, Vector> > pairs;
     
-    if (L1.intersect(L2)) {
-        L1.intersection_point(L2).print_info();
-        printf("%lfx+%lfy+%lf=0\n", L1.a, L1.b, L1.c);
-        printf("%lfx+%lfy+%lf=0\n", L2.a, L2.b, L2.c);
-    } else {
-        printf("no intersection point or too many points");
+    ClosestPair(int size): PointSet(size) {}
+    
+    void sort_points() {
+        sort(P.begin(), P.end(), [] (Vector &a, Vector &b) {
+            return a.x == b.x ? a.y < b.y : a.x < b.x;
+        });
+        P.erase(unique(P.begin(), P.end()), P.end());
+    }
+    void sweeping() {
+        //sort_points();
+        set<Vector> SET = {P[0], P[1]};
+        
+        pairs = {{P[0], P[1]}};
+        ll min = (P[0]-P[1]).size2();
+        for (int i = 2, j = 0; i < P.size(); ++i) {
+            Vector I = P[i];
+            for (; j < i; ++j) {
+                Vector J = P[j];
+                Vector tmp = I-J;
+                if (tmp.x * tmp.x < min || tmp.y * tmp.y < min) break;
+                SET.erase(J);
+            }
+            
+            ll d = sqrt(min) + 1;
+            Vector D = {d, d};
+            auto end = SET.upper_bound(I+D);
+            for (auto it = SET.lower_bound(I-D); it != end; ++it) {
+                Vector J = *it;
+                ll tmp = (I-J).size2();
+                
+                if (tmp < min) {
+                    pairs = {{I,J}};
+                    min = tmp;
+                } else if (tmp == min) {
+                    pairs.push_back({I,J});
+                }
+            }
+            SET.insert(I);
+        }
     }
     
-    return 0;
-}
+    void brute_force() {
+        pairs = {{P[0], P[1]}};
+        ll min = (P[0]-P[1]).size2();
+        for (int i = 0; i < P.size(); ++i) {
+            Vector I = P[i];
+            for (int j = i + 1; j < P.size(); ++j) {
+                Vector J = P[j];
+                ll tmp = (I-J).size2();
+                if (min > tmp) {
+                    pairs = {{I, J}};
+                    min = tmp;
+                } else if (min == tmp) {
+                    pairs.push_back({I, J});
+                }
+            }
+        }
+    }
+};
+
+struct BOJ2261 {
+    int N;
+    ClosestPair close = NULL;
+    
+    BOJ2261() {
+        cin >> N;
+        close = ClosestPair(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> close.P[i].x >> close.P[i].y;
+        }
+    }
+    void solve() {
+        //close.sort_points();
+        //close.sweeping();
+        close.brute_force();
+        auto pairs = close.pairs;
+        cout << (pairs[0].first-pairs[0].second).size2() <<"\n";
+    }
+};
