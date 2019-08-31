e6bb5bd394db9e58910c4758f77ca08e68de33ca
이재영 <47062453+jmslon@users.noreply.github.com>
2019-09-01 02:41:58 +0900

    Update graph.cpp

--- myalgo/graph.cpp
+++ myalgo/graph.cpp
@@ -10,30 +10,40 @@ using namespace std;
 typedef long long ll;
 
 struct Edge {
-    int src, dst; ll cost;
+    int src, dst; ll cost; unsigned long key;
     bool operator > (const Edge &a) const {return cost > a.cost;} // for MIN_HEAP
     Edge operator + (const Edge &a) const {return {src, a.dst, cost+a.cost};} // for Edge NEW
 };
 
 struct Graph {
     int V;
-    vector<vector<unsigned long>> adj;
+    vector<vector<unsigned long>> adj, radj;
     vector<Edge> edges;
     
     Graph(int size) {
         V = size;
         adj.resize(size);
+        radj.resize(size);
     }
     
-    void push(Edge e) {
-        adj[e.src].push_back(edges.size());
-        edges.push_back(e);
+    void push(int src, int dst, ll cost) {
+        push(src, dst, cost, edges.size());
+    }
+    
+    void push(int src, int dst, ll cost, unsigned long key) {
+        adj[src].push_back(key);
+        radj[dst].push_back(key);
+        edges.push_back({src, dst, cost, key});
+    }
+    
+    void update(unsigned long key, ll cost) {
+        edges[key].cost = cost;
     }
 };
 
 struct FloydWashall: Graph {
     FloydWashall(int size): Graph(size) {}
-    vector<vector<ll>> shortest_path() {
+    vector<vector<ll>> shortest_dist() {
         vector<vector<ll>> dist(V, vector<ll>(V, INF));
         
         for (int s = 1; s < V; ++s) {
@@ -60,7 +70,7 @@ struct FloydWashall: Graph {
 
 struct Dijkstra: Graph {
     Dijkstra(int size): Graph(size) {}
-    vector<ll> shortest_path(int s) {
+    vector<ll> shortest_dist(int s) {
         vector<ll> dist(V, INF);
         priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
         
@@ -73,13 +83,32 @@ struct Dijkstra: Graph {
             for (auto idx: adj[curr.dst]) {
                 auto next = edges[idx];
                 Edge NEW = curr + next;
-                if (dist[NEW.dst] < NEW.cost) continue;
-                if (dist[NEW.dst] == NEW.cost) continue;
-                dist[NEW.dst] = NEW.cost;
-                pq.push(NEW);
+                if (dist[NEW.dst] > NEW.cost) {
+                    dist[NEW.dst] = NEW.cost;
+                    pq.push(NEW);
+                }
             }
         }
         return dist;
+    }
+    
+    Graph shortest_path(int s, int d) {
+        auto dist = shortest_dist(s);
+        Graph ret = V;
+        
+        queue<int> q; q.push(d);
+        while (!q.empty()) {
+            auto dst = q.front(); q.pop();
+            for (auto idx: radj[dst]) {
+                auto src = edges[idx].src;
+                if (dist[src] + edges[idx].cost == dist[dst]) {
+                    q.push(src);
+                    ret.push(src, dst, edges[idx].cost, edges[idx].key);
+                }
+            }
+        }
+        
+        return ret;
     }
 };
 
@@ -87,7 +116,7 @@ struct SPFA: Graph {
     vector<ll> dist;
     vector<bool> called;
     SPFA(int size): Graph(size),dist(size, INF){}
-    vector<ll> shortest_path(int s) {
+    vector<ll> shortest_dist(int s) {
         vector<ll> dist = this->dist;
         vector<int> cnt(V, 0);
         vector<bool> inq(V, 0);
@@ -134,107 +163,3 @@ struct SPFA: Graph {
             dfs(edges[idx].dst);
     }
 };
-
-struct BOJ1753 {
-    int V, E, S;
-    Dijkstra dijkstra = 0;
-    BOJ1753() {
-        cin >> V >> E >> S;
-        dijkstra = Dijkstra(V+1);
-        for (int i = 0, u, v, w; i < E; ++i) {
-            cin >> u >> v >> w;
-            dijkstra.push({u, v, w});
-        }
-    }
-    void solve() {
-        auto dist = dijkstra.shortest_path(S);
-        if (dist.empty()) cout << "-1\n";
-        else {
-            for (int i = 1; i <= V; ++i) {
-                if (dist[i] >= INF) cout << "INF\n";
-                else cout << dist[i] << "\n";
-            }
-        }
-    }
-};
-
-struct BOJ11657 {
-    int V, E;
-    SPFA spfa = 0;
-    BOJ11657() {
-        cin >> V >> E;
-        spfa = SPFA(V+1);
-        for (int i = 0, u, v, w; i < E; ++i) {
-            cin >> u >> v >> w;
-            spfa.push({u, v, w});
-        }
-    }
-    void solve() {
-        auto dist = spfa.shortest_path(1);
-        if (dist.empty()) cout << "-1\n";
-        else {
-            for (int i = 2; i <= V; ++i) {
-                if (dist[i] >= INF) cout << "-1\n";
-                else cout << dist[i] << "\n";
-            }
-        }
-    }
-};
-
-struct BOJ1389 {
-    int N, M;
-    FloydWashall floyd = 0;
-    BOJ1389() {
-        cin >> N >> M;
-        floyd = FloydWashall(N+1);
-        for (int i = 0, u, v; i < M; ++i) {
-            cin >> u >> v;
-            floyd.push({u, v, 1});
-            floyd.push({v, u, 1});
-        }
-    }
-    void solve() {
-        auto dist = floyd.shortest_path();
-        vector<int> bacon(N+1, 0);
-        for (int i = 1; i <= N; ++i) {
-            for (int j = 1; j <= N; ++j) {
-                if (i == j) continue;
-                bacon[i] += dist[i][j];
-            }
-        }
-        int answer = 1;
-        for (int i = 2; i <= N; ++i) {
-            if (bacon[i] < bacon[answer])
-                answer = i;
-        }
-        cout << answer << "\n";
-    }
-};
-
-struct BOJ1219 {
-    int N, S, T, M;
-    SPFA spfa = 0;
-    BOJ1219() {
-        cin >> N >> S >> T >> M;
-        spfa = SPFA(N);
-        for (int i = 0, src, dst, cost; i < M; ++i) {
-            cin >> src >> dst >> cost;
-            spfa.push({src, dst, cost});
-        }
-        vector<int> money(N);
-        for (int i = 0; i < N; ++i)
-            cin >> money[i];
-        for (int i = 0; i < M; ++i)
-            spfa.edges[i].cost -= money[spfa.edges[i].dst];
-        auto dist = spfa.shortest_path(S);
-        if (dist[T] == INF) cout << "gg\n";
-        else if (dist[T] == -INF) cout << "Gee\n";
-        else cout << -dist[T]+money[S] << "\n";
-    }
-};
-
-int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    BOJ1219 p;// p.solve();
-    return 0;
-}
