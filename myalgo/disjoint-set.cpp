253d23ffa4a25da182c50adcc0489ac8fcd72b32
이재영 <47062453+jmslon@users.noreply.github.com>
2019-09-06 20:21:39 +0900

    Update disjoint-set.cpp

--- myalgo/disjoint-set.cpp
+++ myalgo/disjoint-set.cpp
@@ -7,21 +7,21 @@ using namespace std;
 typedef long long ll;
 
 struct DisjointSet{
-    ll size;
-    vector<ll> par;
+    int size;
+    vector<int> par;
     
-    DisjointSet(ll size) {
+    DisjointSet(int size) {
         this->size = size;
-        par.resize(size+1);
-        for(ll i = 0; i <= size ; ++i) par[i] = i;
+        par.resize(size);
+        for(int i = 0; i < size ; ++i) par[i] = i;
     }
     
-    ll find(ll x) {
+    ll find(int x) {
         if (par[x] == x) return x;
         return par[x] = find(par[x]);
     }
     
-    void join(ll x, ll y) {
+    void join(int x, int y) {
         x = find(x);
         y = find(y);
         if (x == y) return;
