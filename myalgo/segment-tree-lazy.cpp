0aaf923970ac8202ab55270712a030e4c210e041
이재영 <47062453+jmslon@users.noreply.github.com>
2019-09-17 21:53:49 +0900

    Update segment-tree-lazy.cpp

--- myalgo/segment-tree-lazy.cpp
+++ myalgo/segment-tree-lazy.cpp
@@ -1,10 +1,8 @@
-/*
- boj.10999.cpp
- Sum Segment Tree by value with Lazy Propagation
-*/
-
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 #define l_node (node<<1)
 #define r_node (l_node+1)
@@ -12,88 +10,120 @@
 
 using namespace std;
 
-struct SegmentTree {
-    vector<long long> arr;
-    vector<long long> tree, lazy;
-    int MEANINGLESS;
-    
-    SegmentTree(int size) {
-        arr.resize(size+1);
+typedef long long ll;
+
+struct MaxSegmentTree { // Max Lazy
+   vector<ll> arr, tree, lazy;
+   MaxSegmentTree(int size) {
+       arr.resize(size);
+       tree.resize(size<<2);
+       lazy.resize(size<<2);
+   }
+
+   ll update(int node, int begin, int end, int l_pos, int r_pos, ll dif) {
+       propagate(node, begin, end, lazy[node]);
+       lazy[node] = 0;
+       if (r_pos < begin || end < l_pos) return tree[node];
+       if (l_pos <= begin && end <= r_pos) {
+           propagate(node, begin, end, dif);
+           return tree[node];
+       }
+       ll l = update(l_node, begin, mid, l_pos, r_pos, dif);
+       ll r = update(r_node, mid+1, end, l_pos, r_pos, dif);
+       return tree[node] = l>r?l:r;
+   }
+
+   void propagate(int node, int begin, int end, ll dif) {
+       if (dif == 0) return;
+       tree[node] += dif;
+       if (begin ^ end) {
+           lazy[l_node] += dif;
+           lazy[r_node] += dif;
+       }
+   }
+
+   ll query(int node, int begin, int end, int l_pos, int r_pos) {
+       propagate(node, begin, end, lazy[node]);
+       lazy[node] = 0;
+       if (r_pos < begin || end < l_pos) return 0;
+       if (l_pos <= begin && end <= r_pos) return tree[node];
+       ll l = query(l_node, begin, mid, l_pos, r_pos);
+       ll r = query(r_node, mid+1, end, l_pos, r_pos);
+       return l>r?l:r;
+   }
+};
+
+struct SumSegmentTree { // Sum Lazy
+    vector<ll> arr, tree, lazy;
+    SumSegmentTree(int size) {
+        arr.resize(size);
         tree.resize(size<<2);
         lazy.resize(size<<2);
     }
     
-    long long init(int node, int begin, int end) {
+    ll init(int node, int begin, int end) {
         if (begin == end) return tree[node] = arr[begin];
-        long long l_init = init(l_node, begin, mid);
-        long long r_init = init(r_node, mid+1, end);
-        return tree[node] = whichof(l_init, r_init);
+        ll l = init(l_node, begin, mid);
+        ll r = init(r_node, mid+1, end);
+        return tree[node] = l+r;
     }
     
-    long long update_range(int node, int begin, int end, int l_pos, int r_pos, long long diff) {
+    ll update(int node, int begin, int end, int l_pos, int r_pos, ll dif) {
         propagate(node, begin, end, lazy[node]);
-        lazy[node] = MEANINGLESS;
+        lazy[node] = 0;
         if (r_pos < begin || end < l_pos) return tree[node];
         if (l_pos <= begin && end <= r_pos) {
-            propagate(node, begin, end, diff);
+            propagate(node, begin, end, dif);
             return tree[node];
         }
-        long long l_update = update_range(l_node, begin, mid, l_pos, r_pos, diff);
-        long long r_update = update_range(r_node, mid+1, end, l_pos, r_pos, diff);
-        return tree[node] = whichof(l_update, r_update);
+        ll l = update(l_node, begin, mid, l_pos, r_pos, dif);
+        ll r = update(r_node, mid+1, end, l_pos, r_pos, dif);
+        return tree[node] = l+r;
     }
     
-    long long query(int node, int begin, int end, int l_pos, int r_pos) {
-        propagate(node, begin, end, lazy[node]);
-        lazy[node] = MEANINGLESS;
-        if (r_pos < begin || end < l_pos) return MEANINGLESS;
-        if (l_pos <= begin && end <= r_pos) return tree[node];
-        long long l_query = query(l_node, begin, mid, l_pos, r_pos);
-        long long r_query = query(r_node, mid+1, end, l_pos, r_pos);
-        return whichof(l_query, r_query);
-    }
-    
-    virtual long long whichof(long long l, long long r) = 0;
-    virtual void propagate(int node, int begin, int end, long long diff) = 0;
-};
-
-struct SumSegmentTree : public SegmentTree {
-    SumSegmentTree(int size): SegmentTree(size) {
-        MEANINGLESS = 0;
-    }
-    
-    virtual long long whichof(long long l, long long r) {
-        return (l + r);
-    }
-    
-    virtual void propagate(int node, int begin, int end, long long dif) {
-        tree[node] += (end - begin +1) * dif;
+    void propagate(int node, int begin, int end, ll dif) {
+        if (dif == 0) return;
+        tree[node] += (ll)(end-begin+1)*dif;
         if (begin ^ end) {
             lazy[l_node] += dif;
             lazy[r_node] += dif;
         }
     }
+    
+    ll query(int node, int begin, int end, int l_pos, int r_pos) {
+        propagate(node, begin, end, lazy[node]);
+        lazy[node] = 0;
+        if (r_pos < begin || end < l_pos) return 0;
+        if (l_pos <= begin && end <= r_pos) return tree[node];
+        ll l = query(l_node, begin, mid, l_pos, r_pos);
+        ll r = query(r_node, mid+1, end, l_pos, r_pos);
+        return l+r;
+    }
+};
+
+struct BOJ10999 {
+    BOJ10999() {
+        int N, M, K; cin >> N >> M >> K;
+        SumSegmentTree sum_tree(N+1);
+        for (int i = 1; i <= N; ++i)
+            cin >> sum_tree.arr[i];
+        sum_tree.init(1, 1, N);
+        for (int i = 0; i < M+K; ++i) {
+            int a; cin >> a;
+            if (a == 1) {
+                int b, c; ll d; cin >> b >> c >> d;
+                sum_tree.update(1, 1, N, b, c, d);
+            } else if (a == 2) {
+                int b, c; cin >> b >> c;
+                cout << sum_tree.query(1, 1, N, b, c) << "\n";
+            }
+        }
+    }
 };
 
 int main() {
-    int N, M, K;
-    scanf("%d%d%d", &N, &M, &K);
-    SumSegmentTree segment_tree(N);
-    for (int i = 1; i <= N; ++i)
-        scanf("%lld", &segment_tree.arr[i]);
-    
-    segment_tree.init(1, 1, N);
-    for (int i = 0; i < M+K; ++i) {
-        int a, b, c; long long d;
-        scanf("%d", &a);
-        if (a == 1) { // update
-            scanf("%d%d%lld", &b, &c, &d);
-            segment_tree.update_range(1, 1, N, b, c, d);
-        } else if (a == 2) { // query
-            scanf("%d%d", &b, &c);
-            printf("%lld\n", segment_tree.query(1, 1, N, b, c));
-        }
-    }
-    
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    BOJ10999 p;
     return 0;
 }
+
