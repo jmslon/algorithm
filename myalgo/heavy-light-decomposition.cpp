ace6537c583e2f0e86be7f241df1516ca5388922
이재영 <47062453+jmslon@users.noreply.github.com>
2019-09-24 03:10:54 +0900

    Update heavy-light-decomposition.cpp

--- myalgo/heavy-light-decomposition.cpp
+++ myalgo/heavy-light-decomposition.cpp
@@ -14,101 +14,6 @@ typedef long long ll;
 
 using namespace std;
 
-struct SegmentTree {
-    vector<ll> tree;
-    ll update(int node, int begin, int end, int pos, ll val) {
-        if (pos < begin || end < pos) return tree[node];
-        if (begin == end) return tree[node] = val;
-        ll l_update = update(l_node, begin, mid, pos, val);
-        ll r_update = update(r_node, mid+1, end, pos, val);
-        return tree[node] = l_update + r_update;
-    }
-    ll query(int node, int begin, int end, int l_pos, int r_pos) {
-        if (r_pos < begin || end < l_pos) return 0;
-        if (l_pos <= begin && end <= r_pos) return tree[node];
-        ll l_query = query(l_node, begin, mid, l_pos, r_pos);
-        ll r_query = query(r_node, mid+1, end, l_pos, r_pos);
-        return l_query + r_query;
-    }
-};
-
-struct Edge {
-    int c, p, w;
-    bool operator < (const Edge &a) const {
-        return c < a.c;
-    }
-};
-
-struct HLD {
-    int size = 0;
-    vector<Edge> e;
-    vector<int> c, h, p, f;
-    vector<vector<int> > adj;
-    SegmentTree st;
-    
-    HLD(int size) {
-        // size == N+1
-        // this->size == N
-        e.resize(size);
-        c.resize(size);
-        h.resize(size);
-        p.resize(size);
-        f.resize(size);
-        adj.resize(size);
-        st.tree.resize(size<<2);
-    }
-    
-    int traverse1(int root) {
-        c[root] = 1;
-        for (int child : adj[root]) {
-            if (child ^ p[root]) {
-                p[child] = root;
-                c[root] += traverse1(child);
-            }
-        }
-        return c[root];
-    }
-    
-    void traverse2(int root) {
-        int first = 0;
-        for (int child : adj[root])
-            if (child ^ p[root]&& c[first] < c[child])
-                first = child;
-        for (int child : adj[root])
-            if (child ^ p[root] && child ^ first)
-                traverse2(child);
-        if (!h[root])
-            h[root] = root;
-        if (first) {
-            h[first] = h[root];
-            traverse2(first);
-        }
-        f[root] = ++size;
-    }
-    
-    void init() {
-        for (int i = 1; i < size; ++i) {
-            if (p[e[i].p] == e[i].c) swap(e[i].c, e[i].p);
-            st.update(1, 1, size-1, f[e[i].c], e[i].w);
-        }
-        sort(e.begin(), e.end());
-    }
-    
-    ll query(int s, int d) {
-        if (f[s] > f[d]) swap(s, d);
-        if (h[s] == h[d]) return st.query(1, 1, size-1, f[s], f[d]-1);
-        return query(s, h[s]) + query(p[h[s]], d) + e[h[s]].w;
-    }
-    
-    int lca(int s, int d) {
-        if (f[s] > f[d]) swap(s, d);
-        if (h[s] == h[d]) return d;
-        return lca(p[h[s]], d);
-    }
-};
-
-///
-
 struct Graph {
     vector<vector<int>> adj;
     void push(int src, int dst) {
@@ -116,7 +21,7 @@ struct Graph {
     }
 };
 
-struct HeavyLightDecomposition: Graph {
+struct HeavyLightDecomposition: Graph { // Min
     int size = 0;
     vector<int> c, h, p, f;
     vector<ll> tree;
@@ -250,6 +155,119 @@ struct BOJ13309 {
                     hld.update(c, 0);
                 }
             }
+        }
+    }
+};
+
+
+struct Graph {
+    struct Edge {
+        int src, dst; ll w;
+    };
+    vector<Edge> edges;
+    vector<vector<int>> adj;
+    void push(int src, int dst, ll cst) {
+        adj[src].push_back(dst);
+        adj[dst].push_back(src);
+        edges.push_back({src, dst, cst});
+    }
+};
+
+struct HeavyLightDecomposition: Graph { // Sum
+    int size = 0;
+    vector<int> c, h, p, f;
+    vector<ll> tree;
+    
+    HeavyLightDecomposition(int size) {
+        c.resize(size, 1);
+        h.resize(size);
+        p.resize(size);
+        f.resize(size);
+        adj.resize(size);
+    }
+    
+    void init(int root) {
+        traverse1(root);
+        traverse2(root);
+        tree.resize(size<<2);
+        for (auto edge: edges) {
+            if (p[edge.dst] == edge.src) update(1, 0, size-2, f[edge.dst], edge.w);
+            else update(1, 0, size-2, f[edge.src], edge.w);
+        }
+    }
+    
+    void update(int pos, ll val) {
+        update(1, 0, size-2, f[pos], val);
+    }
+    
+    ll query(int s, int d) {
+        if (f[s] > f[d]) swap(s, d);
+        if (h[s] == h[d]) return query(1, 0, size-2, f[s], f[d]-1);
+        return query(1, 0, size-2, f[s], f[h[s]])+query(p[h[s]], d);
+    }
+    
+private:
+    
+    int traverse1(int root) {
+        c[root] = 1;
+        for (int child : adj[root]) {
+            if (child ^ p[root]) {
+                p[child] = root;
+                c[root] += traverse1(child);
+            }
+        }
+        return c[root];
+    }
+    
+    void traverse2(int root) {
+        int first = 0;
+        for (int child : adj[root])
+            if (child ^ p[root]&& c[first] < c[child])
+                first = child;
+        for (int child : adj[root])
+            if (child ^ p[root] && child ^ first)
+                traverse2(child);
+        if (!h[root])
+            h[root] = root;
+        if (first) {
+            h[first] = h[root];
+            traverse2(first);
+        }
+        f[root] = size++;
+    }
+    
+    ll update(int node, int begin, int end, int pos, ll val) {
+        if (pos < begin || end < pos) return tree[node];
+        if (begin == end) return tree[node] = val;
+        ll l = update(l_node, begin, mid, pos, val);
+        ll r = update(r_node, mid+1, end, pos, val);
+        return tree[node] = l+r;
+    }
+    
+    ll query(int node, int begin, int end, int l_pos, int r_pos) {
+        if (r_pos < begin || end < l_pos) return 0;
+        if (l_pos <= begin && end <= r_pos) return tree[node];
+        ll l = query(l_node, begin, mid, l_pos, r_pos);
+        ll r = query(r_node, mid+1, end, l_pos, r_pos);
+        return l+r;
+    }
+};
+
+struct BOJ1761 {
+    BOJ1761() {
+        int N, M; cin >> N;
+        HeavyLightDecomposition hld = N+1;
+        for (int i = 1; i < N; ++i) {
+            int u, v; ll w; cin >> u >> v >> w;
+            hld.push(u, v, w);
+        }
+        
+        hld.init(1);
+        
+        cin >> M;
+        for (;M--;) {
+            int s, d; cin >> s >> d;
+            cout << hld.query(s, d) << "\n";
         }
     }
 };
