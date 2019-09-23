d691732401e783ccdaccbaeab133aacbbc420626
이재영 <47062453+jmslon@users.noreply.github.com>
2019-09-23 22:18:57 +0900

    Update grid-compress.cpp

--- myalgo/grid-compress.cpp
+++ myalgo/grid-compress.cpp
@@ -103,7 +103,7 @@ struct BOJ5480 {
     }
 };
 
-struct SegmentTree { // Max Lazy Compressed
+struct SegmentTree { // Max Lazy Compressed (Plane)
     vector<ll> arr, tree, lazy;
     
     void init() {
@@ -205,6 +205,107 @@ struct BOJ8889 {
             answer = max(answer, seg_tree.query(min_val, max_val));
         }
         cout << answer << endl;
+    }
+};
+
+struct SegmentTree { // Max Lazy Compressed
+    vector<ll> arr, tree, lazy;
+    
+    void init() {
+        sort(arr.begin(), arr.end());
+        arr.erase(unique(arr.begin(), arr.end()), arr.end());
+        tree.resize(arr.size()*4);
+        lazy.resize(arr.size()*4);
+    }
+    
+    void update(int l_pos, int r_pos, ll dif) {
+        l_pos = (int) (lower_bound(arr.begin(), arr.end(), l_pos)-arr.begin());
+        r_pos = (int) (lower_bound(arr.begin(), arr.end(), r_pos)-arr.begin());
+        update(1, 0, (int) arr.size()-1, l_pos, r_pos, dif);
+    }
+    
+    ll query(int l_pos, int r_pos) {
+        l_pos = (int) (lower_bound(arr.begin(), arr.end(), l_pos)-arr.begin());
+        r_pos = (int) (lower_bound(arr.begin(), arr.end(), r_pos)-arr.begin());
+        return query(1, 0, (int) arr.size()-1, l_pos, r_pos);
+    }
+    
+private:
+    ll update(int node, int begin, int end, int l_pos, int r_pos, ll dif) {
+        propagate(node, begin, end, lazy[node]);
+        lazy[node] = 0;
+        if (r_pos < begin || end < l_pos) return tree[node];
+        if (l_pos <= begin && end <= r_pos) {
+            propagate(node, begin, end, dif);
+            return tree[node];
+        }
+        ll l = update(l_node, begin, mid, l_pos, r_pos, dif);
+        ll r = update(r_node, mid+1, end, l_pos, r_pos, dif);
+        return tree[node] = l>r?l:r;
+    }
+    
+    ll query(int node, int begin, int end, int l_pos, int r_pos) {
+        propagate(node, begin, end, lazy[node]);
+        lazy[node] = 0;
+        if (r_pos < begin || end < l_pos) return 0;
+        if (l_pos <= begin && end <= r_pos) return tree[node];
+        ll l = query(l_node, begin, mid, l_pos, r_pos);
+        ll r = query(r_node, mid+1, end, l_pos, r_pos);
+        return l>r?l:r;
+    }
+    
+    void propagate(int node, int begin, int end, ll dif) {
+        if (dif == 0) return;
+        tree[node] += dif;
+        if (begin ^ end) {
+            lazy[l_node] += dif;
+            lazy[r_node] += dif;
+        }
+    }
+};
+
+struct BOJ16357 {
+    BOJ16357() {
+        struct Node {
+            int src, dst;
+            bool operator < (Node a) const { // for priority_queue
+                // dst가 작은게 가장 먼저 pop
+                return dst == a.dst ? src > a.src : dst > a.dst;
+            }
+        };
+        int N; cin >> N;
+        vector<Node> nodes(N);
+        SegmentTree seg_tree;
+        int first = 10000000, last = -10000000;
+        for (int i = 0; i < N; ++i) {
+            int ux, uy, vx, vy; cin >> ux >> uy >> vx >> vy;
+            nodes[i] = {vy, uy};
+            seg_tree.arr.push_back(vy);
+            seg_tree.arr.push_back(uy);
+            first = min(first, vy);
+            last = max(last, uy);
+        }
+        sort(nodes.begin(), nodes.end(), [] (Node a, Node b) {
+            return a.src == b.src ? a.dst < b.dst : a.src < b.src;
+        });
+        
+        seg_tree.init();
+        for (auto node: nodes)
+            seg_tree.update(node.src, node.dst, 1);
+        
+        ll answer = seg_tree.query(first, last);
+        priority_queue<Node> pq;
+        for (auto node: nodes) {
+            while (!pq.empty()) {
+                if (pq.top().dst < node.src) pq.pop();
+                else break;
+            }
+            pq.push(node);
+            seg_tree.update(node.src, node.dst, -1);
+            answer = max(answer, (ll)pq.size()+seg_tree.query(first, last));
+        }
+        printf("%lld\n", answer);
+        
     }
 };
 
